22a1751cd97f0763cca3073ecbed68d2e1e02faf
gourava29 <gourava29@gmail.com>
2017-01-29 11:43:51 +0000

    added basic component

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,10 @@
-console.log("This is starter app");
+import 'bootstrap-css-only';
+import 'normalize.css';
+
+import angular from 'angular';
+import AppComponent from './app.component';
+import ComponentsModule from './components/components';
+
+angular.module('app', [
+    ComponentsModule.name
+  ]).component('app', AppComponent);
---
+++ client/app/components/categories/categories.component.js
@@ -0,0 +1,8 @@
+import template from './categories.html';
+import './categories.styl';
+
+const CategoriesComponent = {
+    template
+};
+
+export default CategoriesComponent
